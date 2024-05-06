6a7e4593be4aedc1826878637fe99f71ae1a280f
Bogjenero <bornagjenero1@gmail.com>
2024-05-06 10:03:13 +0200

    Riješeni zadaci

--- main.cpp
+++ main.cpp
@@ -4,17 +4,54 @@
 
 void main_window::on_paint(HDC hdc) 
 {
+	RECT rt;
+	GetClientRect(*this, &rt);
+
+	if (lImage) {
+		Gdiplus::Graphics gr(hdc);
+		Gdiplus::RectF displayRect(rt.left, rt.top, rt.right, rt.bottom);
+		gr.DrawImage(lImage.get(), displayRect);
+
+		Gdiplus::StringFormat t;
+		t.SetAlignment(Gdiplus::StringAlignmentCenter);
+		t.SetLineAlignment(Gdiplus::StringAlignmentFar);
+		Gdiplus::Font font(_T("Arial"), 25, Gdiplus::FontStyleBold);
+		Gdiplus::SolidBrush tForm(Gdiplus::Color::Black);
+		gr.DrawString(fileName.c_str(), -1, &font, displayRect, &t, &tForm);
+		
+
+		displayRect.Width = rt.right - 5;
+		displayRect.Height = rt.bottom - 5;
+		tForm.SetColor(Gdiplus::Color::White);
+		gr.DrawString(fileName.c_str(), -1, &font, displayRect, &t, &tForm);
+	}
 }
 
 void main_window::on_command(int id) 
 {
-	switch (id) 
+	switch (id)
 	{
-		case ID_OPEN:
-			break;
-		case ID_EXIT:
-			DestroyWindow(*this);
-			break;
+	case ID_OPEN:
+	{
+		TCHAR path[MAX_PATH] = _T("");
+		OPENFILENAME ofn{ sizeof OPENFILENAME };
+
+		ofn.hwndOwner = *this;
+		ofn.lpstrFile = path;
+		ofn.nMaxFile = MAX_PATH;
+		ofn.lpstrFilter = _T("Image Files (*.jpg;*.png;*.bmp; *.gif;*.tiff;*.emf)\0*.jpg;*.png;*.bmp; *.gif;*.tiff;*.emf\0All Files (*.*)\0*.*\0");
+		ofn.Flags = OFN_HIDEREADONLY | OFN_FILEMUSTEXIST;
+
+		if (::GetOpenFileName(&ofn)) {
+			lImage = std::make_unique<Gdiplus::Image>(path);
+			fileName = std::filesystem::path(path).filename();
+			::InvalidateRect(*this, nullptr, TRUE);
+		}
+		break;
+	}
+	case ID_EXIT:
+		DestroyWindow(*this);
+		break;
 	}
 }
 
